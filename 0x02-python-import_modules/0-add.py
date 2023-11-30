4acbe3df8e53374602a94d0ed1320bed58afee50
HarmonyClexx <harmonymupindu@gmail.com>
2023-11-30 02:57:48 -0800

    correcting

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-add_0 = __import__('add_0').add
+if __name__ == "__main__":
+    from add_0 import add
 
-print("{:d} + {:d} = {:d}".format(a, b, add_0(a, b)))
-
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
